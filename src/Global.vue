9dace45a2d055d1da24c7c0dc10fab59241f5986
liangjiegao <1445808283@qq.com>
2021-09-19 15:05:52 +0800

    a

--- src/Global.vue
+++ src/Global.vue
@@ -1,1 +1,7 @@
-a
+<script>
+const token = 'mc_boao_359436956300976e6cceb00f0e5cbf8d'
+export default
+{
+  token,//用户token身份
+}
+</script>
--- src/user.vue
+++ src/user.vue
@@ -21,9 +21,6 @@
         </el-dropdown-menu>
       </el-dropdown>
 
-
-
-
       <el-button type="success" @click="addUserRole" >确认添加</el-button>
     </div>
     <el-table
@@ -83,117 +80,104 @@
 <script>
 import {Message} from 'element-ui'
 
-var user = [];
-var role = [];
-var totalPage = 0;
+var user = []
+var role = []
+var totalPage = 0
 export default {
-  created(){
-    this.getData(1);
-    this.getRole();
+  created () {
+    this.getData(1)
+    this.getRole()
   },
   data () {
     return {
       tableData: user,
       roleData: role,
-      totalPage:totalPage,
+      totalPage: totalPage,
       input1: '',
-      input2: '',
+      input2: ''
     }
   },
   methods: {
-    editUserRole(row){
-      this.input1 = row.user_key;
-      this.input2 = row.r_key;
+    editUserRole (row) {
+      this.input1 = row.user_key
+      this.input2 = row.r_key
     },
-    addUserRole(e){
+    addUserRole (e) {
       this.$axios({
         method: 'post',
         url: '/user_role/addUserRoleOne',
         data: {
-          token: 'mc_boao_a47b1c55f7f22d610e05f0907a809b73',
+          // eslint-disable-next-line no-undef
+          token: this.GLOBAL.token,
           get_user_key: this.input1,
-          r_key: this.input2,
+          r_key: this.input2
         },
-        timeout: 10000,
+        timeout: 10000
 
-      }).then((response) =>{
-        Message.success(response.data.msg);
+      }).then((response) => {
+        Message.success(response.data.msg)
 
-        if (response.data.code === 10000){
-          this.input1 = '';
-          this.input2 = '';
-          user = [];
+        if (response.data.code === 10000) {
+          this.input1 = ''
+          this.input2 = ''
+          user = []
           this.getData(this.currentPage)
         }
-
-      });
+      })
     },
-    current_change:function(currentPage){ // 下一页
-      user = [];
+    current_change: function (currentPage) { // 下一页
+      user = []
 
-      this.getData(currentPage);
+      this.getData(currentPage)
     },
-    getData(page){
-      this.currentPage = page;
+    getData (page) {
+      this.currentPage = page
       this.$axios({
-        method:'get',
-        url:'/user_role/getURList?page=' + page +'&count=10&token=mc_boao_a47b1c55f7f22d610e05f0907a809b73',
-        data:this.qs.stringify({    //这里是发送给后台的数据
-          userId:this.userId,
-          token:this.token,
-        })
-      }).then((response) =>{          //这里使用了ES6的语法
-        Message.info(response.data.msg);
-        if (response.data.code === 10000){
-          var data = response.data.data;
+        method: 'get',
+        url: '/user_role/getURList?page=' + page + '&count=10&token='.token
+      }).then((response) => { // 这里使用了ES6的语法
+        Message.info(response.data.msg)
+        if (response.data.code === 10000) {
+          var data = response.data.data
           var list = data.list
-          this.totalPage = data.count;
-          list.forEach(function(item) {
+          this.totalPage = data.count
+          list.forEach(function (item) {
             user.push({
               user_key: item.user_key,
               nickname: item.nickname,
               r_name: item.r_name,
-              r_key: item.r_key,
+              r_key: item.r_key
             })
           })
-          this.tableData  = user
+          this.tableData = user
         }
-
-
-      }).catch((error) =>{
-        console.log(error)       //请求失败返回的数据
+      }).catch((error) => {
+        console.log(error) // 请求失败返回的数据
       })
     },
-    getRole(){
+    getRole () {
       this.$axios({
-        method:'get',
-        url:'/role/getRoleList?token=mc_boao_a47b1c55f7f22d610e05f0907a809b73',
-        data:this.qs.stringify({    //这里是发送给后台的数据
-          userId:this.userId,
-          token:this.token,
-        })
-      }).then((response) =>{          //这里使用了ES6的语法
-        Message.info(response.data.msg);
-        if (response.data.code === 10000){
-          var data = response.data.data;
+        method: 'get',
+        url: '/role/getRoleList?token=' + this.GLOBAL.token
+      }).then((response) => { // 这里使用了ES6的语法
+        Message.info(response.data.msg)
+        if (response.data.code === 10000) {
+          var data = response.data.data
           var list = data.list
-          list.forEach(function(item) {
+          list.forEach(function (item) {
             role.push({
               name: item.name,
-              r_key: item.r_key,
+              r_key: item.r_key
             })
           })
-          this.roleData  = role
+          this.roleData = role
         }
-
-
-      }).catch((error) =>{
-        console.log(error)       //请求失败返回的数据
+      }).catch((error) => {
+        console.log(error) // 请求失败返回的数据
       })
     },
-    selectRole(command){
-      this.input2 = command.r_key;
-
+    selectRole (command) {
+      this.input2 = command.r_key
     }
   }
 
